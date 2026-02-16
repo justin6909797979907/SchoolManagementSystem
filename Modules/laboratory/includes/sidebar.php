e491378b82d180265ca23c47492e5f34029e980c
justin <heredajustinq@gmail.com>
2026-02-16 16:15:57 +0800

    Add dropdown menu for Laboratories in sidebar

--- Modules/laboratory/includes/sidebar.php
+++ Modules/laboratory/includes/sidebar.php
@@ -19,6 +19,16 @@
             <li><a href="" class="menu-link">Laboratories</a></li>
             <li><a href="" class="menu-link">Reports</a></li>
 
+            <div class="">
+                <button class="menu-link" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
+                    Laboratories
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                    <li><button class="dropdown-item" type="button">Action</button></li>
+                    <li><button class="dropdown-item" type="button">Another action</button></li>
+                    <li><button class="dropdown-item" type="button">Something else here</button></li>
+                </ul>
+            </div>
             
         </ul>
 
