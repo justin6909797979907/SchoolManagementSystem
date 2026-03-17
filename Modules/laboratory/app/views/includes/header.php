10bd31db64ab93fca623181e6c272c03ff2486a4
Justin <heredajustinq@gmail.com>
2026-03-17 15:28:04 +0800

    feat: update database connection string and improve stylesheet link in header feat: add new image files for uploads

--- Modules/laboratory/app/views/includes/header.php
+++ Modules/laboratory/app/views/includes/header.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/SchoolManagementSystem/Modules/laboratory/css/styles.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/css/styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -36,6 +36,8 @@
             <span></span>
         </div>
     </header>
+
+  
 
     <script src="js/sidebar.js"></script>
 
