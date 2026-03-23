ccc950b67fdd19e0c478754bc8e42da104d62e1e
justin <heredajustinq@gmail.com>
2026-03-23 17:20:42 +0800

    refactor: update sidebar structure and improve readability

--- Modules/laboratory/app/views/includes/sidebar.php
+++ Modules/laboratory/app/views/includes/sidebar.php
@@ -6,16 +6,19 @@
             <i class="fa-regular fa-circle-user"></i>
         </div>
     </div>
+
     <div class="sidebar-header">
         <div class="user-avatar">UA</div>
         <h1>Username</h1>
         <p class="user-id">SAMPLEID123456789</p>
     </div>
+
     <h2>Laboratory</h2>
-    <ul id="accordionSidebar">
-        <li><a href="<?= BASE_URL ?>" class="menu-link <?php echo CURRENT_URI == 'laboratory'? "active" :  "" ?>">Dashboard</a></li>
+    <div id="accordionSidebar">
+        <div><a href="<?= BASE_URL ?>" class="menu-link <?php echo CURRENT_URI == 'laboratory' ? "active" :  "" ?>">Dashboard</a></div>
+
         <!-- Laboratories -->
-        <li class="nav-item">
+        <div class="nav-item">
             <a class="menu-link collapsed d-flex justify-content-between align-items-center"
                 href="#"
                 data-bs-toggle="collapse"
@@ -43,11 +46,11 @@
                     </a>
 
                     <div class="collapse ps-3" id="collapsePhysics">
-                        <a href="<?= BASE_URL ?>/inventory" class="dropdown-item menu-link small text-white <?php echo CURRENT_URI == 'inventory'? "active" :  "" ?>">Inventory</a>
-                        <a href="<?= BASE_URL ?>/damages" class="dropdown-item menu-link small text-white <?php echo CURRENT_URI == 'damages'? "active" :  "" ?>">Damages</a>
-                        <a href="<?= BASE_URL ?>/borrow" class="dropdown-item menu-link small text-white <?php echo CURRENT_URI == 'borrow'? "active" :  "" ?>">Borrows</a>
+                        <a href="<?= BASE_URL ?>/inventory" class="dropdown-item menu-link small text-white <?php echo CURRENT_URI == 'inventory' ? "active" :  "" ?>">Inventory</a>
+                        <a href="<?= BASE_URL ?>/damages" class="dropdown-item menu-link small text-white <?php echo CURRENT_URI == 'damages' ? "active" :  "" ?>">Damages</a>
+                        <a href="<?= BASE_URL ?>/borrow" class="dropdown-item menu-link small text-white <?php echo CURRENT_URI == 'borrow' ? "active" :  "" ?>">Borrows</a>
                     </div>
-                    
+
                     <a class="dropdown-item menu-link text-white d-flex justify-content-between align-items-center collapsed"
                         data-bs-toggle="collapse"
                         href="#collapsePsychology"
@@ -59,8 +62,8 @@
                     </a>
 
                     <div class="collapse ps-3" id="collapsePsychology">
-                        <a href="<?= BASE_URL ?>/psycho-inventory" class="dropdown-item menu-link small text-white <?php echo CURRENT_URI == 'psycho-inventory'? "active" :  "" ?>">Inventory</a>
-                        <a href="<?= BASE_URL ?>/psycho-damage" class="dropdown-item menu-link small text-white <?php echo CURRENT_URI == 'psycho-damage'? "active" :  "" ?>">Damages</a>
+                        <a href="<?= BASE_URL ?>/psycho-inventory" class="dropdown-item menu-link small text-white <?php echo CURRENT_URI == 'psycho-inventory' ? "active" :  "" ?>">Inventory</a>
+                        <a href="<?= BASE_URL ?>/psycho-damage" class="dropdown-item menu-link small text-white <?php echo CURRENT_URI == 'psycho-damage' ? "active" :  "" ?>">Damages</a>
                         <a href="#" class="dropdown-item menu-link small text-white">Borrows</a>
                     </div>
 
@@ -75,13 +78,13 @@
                     </a>
 
                     <div class="collapse ps-3" id="collapseHE">
-                        <a href="<?= BASE_URL ?>/he-inventory" class="dropdown-item menu-link small text-white <?php echo CURRENT_URI == 'he-inventory'? "active" :  "" ?>">Inventory</a>
+                        <a href="<?= BASE_URL ?>/he-inventory" class="dropdown-item menu-link small text-white <?php echo CURRENT_URI == 'he-inventory' ? "active" :  "" ?>">Inventory</a>
                         <a href="#" class="dropdown-item menu-link small text-white">Damages</a>
                         <a href="#" class="dropdown-item menu-link small text-white">Borrows</a>
                     </div>
 
                     <!-- Crim Lab with Submenu -->
-                    <a class="dropdown-item menu-link text-white d-flex justify-content-between align-items-center collapsed"
+                    <!-- <a class="dropdown-item menu-link text-white d-flex justify-content-between align-items-center collapsed"
                         data-bs-toggle="collapse"
                         href="#collapseCrim"
                         role="button"
@@ -92,10 +95,45 @@
                     </a>
 
                     <div class="collapse ps-2 text-white" id="collapseCrim">
-                        <a href="<?= BASE_URL ?>/crim-inventory" class="dropdown-item menu-link small text-white <?php echo CURRENT_URI == 'crim-inventory'? "active" :  "" ?>">Inventory</a>
-                        <a href="<?= BASE_URL ?>/crim-borrow" class="dropdown-item menu-link small fw-light <?php echo CURRENT_URI == 'crim-borrow'? "active" :  "" ?>">Borrows</a>
-                        <a href="<?= BASE_URL ?>/crim-damage" class="dropdown-item menu-link small fw-light <?php echo CURRENT_URI == 'crim-damage'? "active" :  "" ?>">Damages</a>
+                        <a href="<?= BASE_URL ?>/crim-inventory" class="dropdown-item menu-link small text-white <?php echo CURRENT_URI == 'crim-inventory' ? "active" :  "" ?>">Inventory</a>
+                        <a href="<?= BASE_URL ?>/crim-borrow" class="dropdown-item menu-link small fw-light <?php echo CURRENT_URI == 'crim-borrow' ? "active" :  "" ?>">Borrows</a>
+                        <a href="<?= BASE_URL ?>/crim-damage" class="dropdown-item menu-link small fw-light <?php echo CURRENT_URI == 'crim-damage' ? "active" :  "" ?>">Damages</a>
+                    </div> -->
+
+                    <!-- Crim Lab with Submenu -->
+                    <a class="dropdown-item menu-link text-white d-flex justify-content-between align-items-center collapsed"
+                        data-bs-toggle="collapse"
+                        href="#collapseCrim"
+                        role="button">
+                        <span>Crim Lab</span>
+                        <i class="bi bi-chevron-right dropdown-icon fs-6"></i>
+                    </a>
+
+                    <div class="collapse ps-2" id="collapseCrim">
+                        <a href="#" class="dropdown-item menu-link small fw-light small text-white">Finger Print Lab</a>
+                        <a href="#" class="dropdown-item menu-link small fw-light small text-white">Crime Scene Lab</a>
+                        <a href="#" class="dropdown-item menu-link small fw-light small text-white">Ballistics Lab</a>
+                        <a href="#" class="dropdown-item menu-link small fw-light small text-white">Questions Document Lab</a>
+                        <a href="#" class="dropdown-item menu-link small fw-light small text-white">Chemistry Lab</a>
+                        <a href="#" class="dropdown-item menu-link small fw-light small text-white">Defense & Tactics Lab</a>
                     </div>
+
+                    <!-- IT Lab with Submenu -->
+                    <!-- <a class="dropdown-item menu-link text-white d-flex justify-content-between align-items-center collapsed"
+                        data-bs-toggle="collapse"
+                        href="#collapseIT"
+                        role="button"
+                        aria-expanded="false"
+                        aria-controls="collapseIT">
+                        <span>IT Lab</span>
+                        <i class="bi bi-chevron-right dropdown-icon fs-6"></i>
+                    </a>
+
+                    <div class="collapse ps-2" id="collapseIT">
+                        <a href="<?= BASE_URL ?>/it-inventory" class="dropdown-item menu-link small text-white <?php echo CURRENT_URI == 'it-inventory' ? "active" :  "" ?>">Inventory</a>
+                        <a href="#" class="dropdown-item menu-link small fw-light text-white">Damages</a>
+                        <a href="#" class="dropdown-item menu-link small fw-light text-white">Borrows</a>
+                    </div> -->
 
                     <!-- IT Lab with Submenu -->
                     <a class="dropdown-item menu-link text-white d-flex justify-content-between align-items-center collapsed"
@@ -109,18 +147,17 @@
                     </a>
 
                     <div class="collapse ps-2" id="collapseIT">
-                         <a href="<?= BASE_URL ?>/it-inventory" class="dropdown-item menu-link small text-white <?php echo CURRENT_URI == 'it-inventory'? "active" :  "" ?>">Inventory</a>
-                        <a href="#" class="dropdown-item menu-link small fw-light text-white">Damages</a>
-                        <a href="#" class="dropdown-item menu-link small fw-light text-white">Borrows</a>
+                        <a href="#" class="dropdown-item menu-link small fw-light text-white">IT Lab 1</a>
+                        <a href="#" class="dropdown-item menu-link small fw-light text-white">IT Lab 2</a>
+                        <a href="#" class="dropdown-item menu-link small fw-light text-white">IT Lab 3</a>
                     </div>
 
                 </div>
             </div>
-        </li>
-
+        </div>
 
         <!-- Reports -->
-        <li class="nav-item">
+        <div class="nav-item">
             <a class="menu-link collapsed d-flex justify-content-between align-items-center"
                 href="#"
                 data-bs-toggle="collapse"
@@ -142,23 +179,24 @@
                     <a class="dropdown-item menu-link" href="#">Monthly Schedule</a>
                 </div>
             </div>
-        </li>
+        </div>
 
-        <li><a href="" class="menu-link">Users</a></li>
-         <a href="<?= BASE_URL ?>/schedule" class="dropdown-item menu-link small fw-light <?php echo CURRENT_URI == 'schedule'? "active" :  "" ?>">Schedule</a>
-        <li><a href="" class="menu-link">Reports</a></li>
-        <li><a href="<?= BASE_URL ?>/approval-decision-support" class="menu-link">Approval Decision Support</a></li>
-        <li><a href="<?= BASE_URL ?>/concern-issue-tracking" class="menu-link">Concerns Issue Tracking</a></li>
-        <li><a href="<?= BASE_URL ?>/report-submission-management" class="menu-link">Report Submmision Management</a></li>
+        <div><a href="" class="menu-link">Users</a></div>
+        <a href="<?= BASE_URL ?>/schedule" class="dropdown-item menu-link small fw-light <?php echo CURRENT_URI == 'schedule' ? "active" :  "" ?>">Schedule</a>
 
-    </ul>
+        <hr class="text-white">
+        <div class="text-white py-2 px-3 rounded shadow-sm w-100">Reports:</div>
+        <div><a href="<?= BASE_URL ?>/approval-decision-support" class="menu-link">Approval Decision Support</a></div>
+        <div><a href="<?= BASE_URL ?>/concern-issue-tracking" class="menu-link">Concerns Issue Tracking</a></div>
+        <div><a href="<?= BASE_URL ?>/report-submission-management" class="menu-link">Report Submmision Management</a></div>
+
+    </div>
 
 
     <div class="sidebar-header">
     </div>
 
-    <ul>
-        <li><a href="#" class="menu-link">Settings</a></li>
-        <li><a href="#" class="menu-link">Lagout</a></li>
-    </ul>
+    <div><a href="#" class="menu-link">Settings</a></div>
+    <div><a href="#" class="menu-link">Logout</a></div>
+
 </aside>
