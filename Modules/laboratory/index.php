e64a2c6d8254f3de82c5fc5a01e9f5862cfc1b4f
Justin <heredajustinq@gmail.com>
2026-08-02 01:27:18 +0800

    feat: Implement CRUD functionality for Crime Scene Damage management

--- Modules/laboratory/index.php
+++ Modules/laboratory/index.php
@@ -261,9 +261,23 @@ switch ($segments[0] ?? '') {
         (new FingerprintDmgController())->index();
         break;
 
+
     case 'crime-scene-damage':
         require_once __DIR__ . '/app/controllers/CrimeSceneDmgController.php';
-        (new CrimeSceneDmgController())->index();
+        $controller = new CrimeSceneDmgController();
+
+        if (!isset($segments[1])) {
+            $controller->index();
+        } elseif ($segments[1] == 'create') {
+            $controller->create();
+        } elseif ($segments[1] == 'view' && isset($segments[2])) {
+            $controller->view($segments[2]);
+        } elseif ($segments[1] == 'update') {
+            $controller->update();
+        } elseif ($segments[1] == 'delete' && isset($segments[2])) {
+            $controller->delete($segments[2]);
+        }
+
         break;
 
 
