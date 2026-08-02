f828ebaf77e3bc3cdd83514a6c7dcda10a4794e5
Justin <heredajustinq@gmail.com>
2026-08-02 14:07:31 +0800

    feat: Add CRUD functionality for Defense Tactics Damage management

--- Modules/laboratory/app/views/damages/crim/defense-tactics/dtEditDamageModal.php
+++ Modules/laboratory/app/views/damages/crim/defense-tactics/dtEditDamageModal.php
@@ -2,10 +2,13 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
 
-            <form id="dtEditDamageForm" action="<?= BASE_URL ?>/damages/update" method="POST">
+             <form
+                id="dtEditDamageForm"
+                action="<?= BASE_URL ?>/defense-tactics-damage/update"
+                method="POST">
 
                 <!-- Hidden ID -->
-                <input type="hidden" name="id" id="dt_edit_id">
+                <input type="hidden" name="id" id="edit_id">
 
                 <div class="modal-header">
                     <h5 class="modal-title" id="dtEditDamageModalLabel">
@@ -25,7 +28,7 @@
                             <input
                                 type="text"
                                 name="item_name"
-                                id="dt_edit_item_name"
+                                id="edit_item_name"
                                 class="form-control"
                                 required>
                         </div>
@@ -36,7 +39,7 @@
                             <input
                                 type="text"
                                 name="laboratory"
-                                id="dt_edit_laboratory"
+                                id="edit_laboratory"
                                 class="form-control"
                                 value="Defense Tactics Laboratory"
                                 readonly>
@@ -48,7 +51,7 @@
                             <input
                                 type="text"
                                 name="issue"
-                                id="dt_edit_issue"
+                                id="edit_issue"
                                 class="form-control"
                                 required>
                         </div>
@@ -59,7 +62,7 @@
                             <input
                                 type="text"
                                 name="reported_by"
-                                id="dt_edit_reported_by"
+                                id="edit_reported_by"
                                 class="form-control"
                                 required>
                         </div>
@@ -70,7 +73,7 @@
                             <input
                                 type="date"
                                 name="date_reported"
-                                id="dt_edit_date_reported"
+                                id="edit_date_reported"
                                 class="form-control"
                                 required>
                         </div>
@@ -80,7 +83,7 @@
                             <label class="form-label">Status</label>
                             <select
                                 name="status"
-                                id="dt_edit_status"
+                                id="edit_status"
                                 class="form-select"
                                 required>
                                 <option value="" selected disabled>-- Select Status --</option>
--- Modules/laboratory/index.php
+++ Modules/laboratory/index.php
@@ -349,10 +349,25 @@ switch ($segments[0] ?? '') {
         break;
 
 
+
     case 'defense-tactics-damage':
-        require_once __DIR__ . '/app/controllers/DefenseTacticsDmgController.php';
-        (new DefenseTacticsDmgController())->index();
+         require_once __DIR__ . '/app/controllers/DefenseTacticsDmgController.php';
+        $controller = new DefenseTacticsDmgController();
+
+        if (!isset($segments[1])) {
+            $controller->index();
+        } elseif ($segments[1] == 'create') {
+            $controller->create();
+        } elseif ($segments[1] == 'view' && isset($segments[2])) {
+            $controller->view($segments[2]);
+        } elseif ($segments[1] == 'update') {
+            $controller->update();
+        } elseif ($segments[1] == 'delete' && isset($segments[2])) {
+            $controller->delete($segments[2]);
+        }
+
         break;
+
 
     case 'question-document-damage':
         require_once __DIR__ . '/app/controllers/QuestionDocumentDmgController.php';
